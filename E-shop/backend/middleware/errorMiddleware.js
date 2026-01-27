39378dde6e2f14e210300c129a1b1160e42154ba
fish19-12 <155108286+fish19-12@users.noreply.github.com>
2026-01-28 01:38:42 +0300

    Remove duplicated E-shop directory (unused by Render)

--- E-shop/backend/middleware/errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  return res.status(500).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : null,
-  });
-};
