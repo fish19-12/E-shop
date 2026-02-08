eca08f6302f7ede14b29dd3c7ab4a875c9a552bb
fish man <fisehalidetu12@gmail.com>
2026-02-08 12:17:41 +0300

    Fix error middleware

--- E-shop/backend/middleware/errorMiddleware.js
+++ E-shop/backend/middleware/errorMiddleware.js
@@ -1,6 +1,9 @@
 export const errorHandler = (err, req, res, next) => {
-  return res.status(500).json({
-    message: err.message,
+  const statusCode =
+    res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+
+  res.status(statusCode).json({
+    message: err.message || "Server Error",
     stack: process.env.NODE_ENV === "development" ? err.stack : null,
   });
 };
